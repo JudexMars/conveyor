49e19f31db7c10f815eb99d3a3ff6f938fada6d7
Daniel Bondar <danielpro2013rus@gmail.com>
2025-12-26 04:15:51 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("com.gradleup.shadow") version "8.3.5"
+    application
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+description = "conveyor"
+
+application {
+    mainClass.set("org.example.conveyor.ConveyorApplicationKt")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.12")
+    implementation("io.ktor:ktor-server-netty:2.3.12")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+
+    testImplementation("io.ktor:ktor-server-tests:2.3.12")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("conveyor")
+    archiveClassifier.set("all")
+    archiveVersion.set("")
+    manifest {
+        attributes["Main-Class"] = "org.example.conveyor.ConveyorApplicationKt"
+    }
+}
---
+++ src/main/kotlin/org/example/conveyor/ConveyorApplication.kt
@@ -0,0 +1,23 @@
+package org.example.conveyor
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun main() {
+    embeddedServer(Netty, port = 8081, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Привет от Бондарчука!")
+        }
+    }
+}
